e8251a8e84096150f9d7539ae7dff17299f7f91e
neislerw <neislerw@gmail.com>
2025-05-08 20:21:58 -0400

    Added settings menu with threshold settings

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -37,14 +37,17 @@ partial class MainForm
         this.thumbnailPanel = new System.Windows.Forms.FlowLayoutPanel();
         this.deleteButton = new System.Windows.Forms.Button();
         this.cancelButton = new System.Windows.Forms.Button();
+        this.menuStrip = new System.Windows.Forms.MenuStrip();
+        this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
         ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLeft)).BeginInit();
         ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRight)).BeginInit();
         this.thumbnailPanel.SuspendLayout();
+        this.menuStrip.SuspendLayout();
         this.SuspendLayout();
         // 
         // selectFolderButton
         // 
-        this.selectFolderButton.Location = new System.Drawing.Point(20, 20);
+        this.selectFolderButton.Location = new System.Drawing.Point(20, 30);
         this.selectFolderButton.Name = "selectFolderButton";
         this.selectFolderButton.Size = new System.Drawing.Size(150, 30);
         this.selectFolderButton.TabIndex = 0;
@@ -54,7 +57,7 @@ partial class MainForm
         // 
         // pictureBoxLeft
         // 
-        this.pictureBoxLeft.Location = new System.Drawing.Point(20, 60);
+        this.pictureBoxLeft.Location = new System.Drawing.Point(20, 70);
         this.pictureBoxLeft.Name = "pictureBoxLeft";
         this.pictureBoxLeft.Size = new System.Drawing.Size(380, 400);
         this.pictureBoxLeft.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -64,7 +67,7 @@ partial class MainForm
         // 
         // pictureBoxRight
         // 
-        this.pictureBoxRight.Location = new System.Drawing.Point(400, 60);
+        this.pictureBoxRight.Location = new System.Drawing.Point(400, 70);
         this.pictureBoxRight.Name = "pictureBoxRight";
         this.pictureBoxRight.Size = new System.Drawing.Size(380, 400);
         this.pictureBoxRight.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -74,7 +77,7 @@ partial class MainForm
         // 
         // remainingLabel
         // 
-        this.remainingLabel.Location = new System.Drawing.Point(20, 470);
+        this.remainingLabel.Location = new System.Drawing.Point(20, 480);
         this.remainingLabel.Name = "remainingLabel";
         this.remainingLabel.Size = new System.Drawing.Size(760, 30);
         this.remainingLabel.TabIndex = 3;
@@ -83,7 +86,7 @@ partial class MainForm
         // 
         // instructionLabel
         // 
-        this.instructionLabel.Location = new System.Drawing.Point(20, 500);
+        this.instructionLabel.Location = new System.Drawing.Point(20, 510);
         this.instructionLabel.Name = "instructionLabel";
         this.instructionLabel.Size = new System.Drawing.Size(760, 60);
         this.instructionLabel.TabIndex = 4;
@@ -93,7 +96,7 @@ partial class MainForm
         // thumbnailPanel
         // 
         this.thumbnailPanel.AutoScroll = true;
-        this.thumbnailPanel.Location = new System.Drawing.Point(20, 60);
+        this.thumbnailPanel.Location = new System.Drawing.Point(20, 70);
         this.thumbnailPanel.Name = "thumbnailPanel";
         this.thumbnailPanel.Size = new System.Drawing.Size(760, 400);
         this.thumbnailPanel.TabIndex = 5;
@@ -101,7 +104,7 @@ partial class MainForm
         // 
         // deleteButton
         // 
-        this.deleteButton.Location = new System.Drawing.Point(200, 470);
+        this.deleteButton.Location = new System.Drawing.Point(200, 480);
         this.deleteButton.Name = "deleteButton";
         this.deleteButton.Size = new System.Drawing.Size(150, 30);
         this.deleteButton.TabIndex = 6;
@@ -112,7 +115,7 @@ partial class MainForm
         // 
         // cancelButton
         // 
-        this.cancelButton.Location = new System.Drawing.Point(450, 470);
+        this.cancelButton.Location = new System.Drawing.Point(450, 480);
         this.cancelButton.Name = "cancelButton";
         this.cancelButton.Size = new System.Drawing.Size(150, 30);
         this.cancelButton.TabIndex = 7;
@@ -120,6 +123,23 @@ partial class MainForm
         this.cancelButton.UseVisualStyleBackColor = true;
         this.cancelButton.Visible = false;
         this.cancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+        // 
+        // menuStrip
+        // 
+        this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.settingsToolStripMenuItem});
+        this.menuStrip.Location = new System.Drawing.Point(0, 0);
+        this.menuStrip.Name = "menuStrip";
+        this.menuStrip.Size = new System.Drawing.Size(800, 24);
+        this.menuStrip.TabIndex = 8;
+        this.menuStrip.Text = "menuStrip";
+        // 
+        // settingsToolStripMenuItem
+        // 
+        this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+        this.settingsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+        this.settingsToolStripMenuItem.Text = "Settings";
+        this.settingsToolStripMenuItem.Click += new System.EventHandler(this.SettingsToolStripMenuItem_Click);
         // 
         // MainForm
         // 
@@ -133,11 +153,16 @@ partial class MainForm
         this.Controls.Add(this.pictureBoxRight);
         this.Controls.Add(this.pictureBoxLeft);
         this.Controls.Add(this.selectFolderButton);
+        this.Controls.Add(this.menuStrip);
+        this.MainMenuStrip = this.menuStrip;
         this.Text = "Picksy";
         ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLeft)).EndInit();
         ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRight)).EndInit();
         this.thumbnailPanel.ResumeLayout(false);
+        this.menuStrip.ResumeLayout(false);
+        this.menuStrip.PerformLayout();
         this.ResumeLayout(false);
+        this.PerformLayout();
     }
 
     #endregion
@@ -150,4 +175,6 @@ partial class MainForm
     private System.Windows.Forms.FlowLayoutPanel thumbnailPanel;
     private System.Windows.Forms.Button deleteButton;
     private System.Windows.Forms.Button cancelButton;
+    private System.Windows.Forms.MenuStrip menuStrip;
+    private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
 }
